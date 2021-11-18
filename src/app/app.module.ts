4e9cd1892bae566faa0a75abd9f4d754e631cecd
Alex Carrera <a_fcalvarez@hotmail.com>
2021-11-18 17:13:22 -0500

    formularios en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { routing, appRoutingProviders } from './app.routing';
+import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { MiComponente } from './components/mi-componente/mi-componente.component';
@@ -41,8 +42,7 @@ import {EsparPipe} from "./pipes/espar.pipe";
   imports: [
     BrowserModule,
     routing,
-    CommonModule,
-    CommonModule
+    FormsModule
   ],
   providers: [appRoutingProviders],
   bootstrap: [AppComponent]
--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+  public user: any;
+
+  constructor() {
+    this.user = {
+      nombre: '',
+      apellidos: '',
+      bio: '',
+      genero: ''
+    }
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    alert('Formulario Enviado');
+    console.log(this.user)
   }
 
 }
